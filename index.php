8d719be90be7b3ecee57abd0ef691b014968dff7
identidadesinsurrectas <95011668+identidadesinsurrectas@users.noreply.github.com>
2021-11-26 11:00:00 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Identidades insurrectas</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/extras_bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!--<link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">-->
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@500&family=Varela+Round&display=swap" rel="stylesheet">
+
+
+    <meta http-equiv = "refresh" content = "3; url = http://identidadesinsurrectas.miguelmesa.info/web/viewer.html?file=http://identidadesinsurrectas.miguelmesa.info/TDPC1.pdf" />
+   </head>
+   <body>
+
+     <div class="container">
+       <br>
+       <br>
+       <br>
+       <div class="row align-items-center mx-auto">
+         <h4 class="text-center mx-auto">Abriendo documento de Taller de Proyectos Creativos I</h4>
+       </div>
+       <div class="row align-items-center mx-auto">
+         <div class="col-12 text-center">
+           <img src="loading.gif" class="img-fluid">
+         </div>
+       </div>
+       <div class="row align-items-center mx-auto">
+         <br>
+         <br>
+         <h5 class="text-center mx-auto">
+           Si tienes problemas para abrirlo, descarga
+           <a href="TDPC1.pdf">aquí</a>
+         </h5>
+       </div>
+     </div>
+
+   </body>
+
+</html>
